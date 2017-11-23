1d813f4c6387db69b2c77ea7b7c7917b78220b4c
Taikun Guo <billg1990@gmail.com>
2017-11-23 14:49:58 -0500

    Added validation of operations.

---
+++ transaction.py
@@ -0,0 +1,53 @@
+import sys, re
+
+
+class TransactionManager(object):
+    """
+    This module is the entry point of all
+    the transactions, it validate and process them.
+    """
+
+    def __init__(self):
+        """
+        Initialize object, create regex for validating comments and operations
+        """
+        self.REGEX_COMMENT = re.compile(r"(\/\/|===).+")
+        self.REGEX_OPERATIONS = re.compile(r"(begin\(T\d+\)|beginRO\(T\d+\)" \
+            + "|R\(T\d+,x\d+\)|W\(T\d+,x\d+,\d+\)|dump\((\d+|x\d+)?\)|" \
+            + "end\(T\d+\)|fail\(\d+\)|recover\(\d+\))")
+
+    def process(self, line):
+        """
+        validate a line and process all the operations
+        """
+        operations = self._validate(line)
+        if operations is None:
+            print("Line contains invalid operation!")
+        else:
+            self._execute_ops(operations)
+
+    def _validate(self, line):
+        """
+        Check if this line contains a sequence of
+        valid operations.\n
+        If so, returns a list of operations\n
+        If this line is a comment e.g. "//" or "===" then
+        return an empty list\n
+        If invalid, return None
+        """
+        # remove whitespaces since it will cause trouble parsing this line
+        no_whitespaces = "".join(line.split())
+        if self.REGEX_COMMENT.match(no_whitespaces):
+            # check if this line is a comment
+            return []
+        elif self.REGEX_OPERATIONS.match(no_whitespaces):
+            # check if this line is a list of operations
+            operations = [(i.split("(")[0], i.split("(")[0]) \
+                for i in no_whitespaces.split(")")]
+            return operations
+        else:
+            # the operation is not valid, return None
+            return None
+
+    def _execute_ops(self, operations):
+        pass
