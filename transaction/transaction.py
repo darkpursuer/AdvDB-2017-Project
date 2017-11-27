ed40b6d12b453f69db2de87d4a0cf4cad4f1ebbb
Taikun Guo <billg1990@gmail.com>
2017-11-26 19:44:50 -0500

    added transaction.

--- transaction/transaction.py
+++ transaction/transaction.py
@@ -1,10 +1,10 @@
 class Transaction(object):
     """
-    Each transaction has a type and a name.
-    type can be NORMAL or READ means read-only.
-    NORMAL
+    stands for a transaction
     """
 
     def __init__(self, tp, name):
-        self.TYPE = tp
-        self.NAME = name
+        self.type = tp # NORMAL or READ means read-only.
+        self.name = name # e.g. T1
+        self.status = "RUNNING" # RUNNING, BLOCKED, COMMITED, ABORTED
+        self.buffer = [] # pending operations
